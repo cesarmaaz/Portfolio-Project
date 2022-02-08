d6a1296a3273d8a4cbd00f948c20a827d6102c82
Cesar Maaz <cesarmaaz@gmail.com>
2022-02-08 19:32:42 +0200

    portfolio v1

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,109 @@
+import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+import { useContext, useRef, useState } from "react";
+import emailjs from "emailjs-com";
+import { ThemeContext } from "../../context";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [done, setDone] = useState(false);
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_hj0q2wq",
+        "template_xna14ej",
+        formRef.current,
+        "user_hLSiEtk2SiDR9GYBXQpXW"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +96176431500
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Email} alt="" />
+              cesarmaaz@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Address} alt="" />
+              Beirut, Lebanon
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>What’s your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input
+              style={{
+                backgroundColor: darkMode && "#333",
+                color: darkMode && "white",
+              }}
+              type="text"
+              placeholder="Name"
+              name="user_name"
+            />
+            <input
+              style={{
+                backgroundColor: darkMode && "#333",
+                color: darkMode && "white",
+              }}
+              type="text"
+              placeholder="Subject"
+              name="user_subject"
+            />
+            <input
+              style={{
+                backgroundColor: darkMode && "#333",
+                color: darkMode && "white",
+              }}
+              type="text"
+              placeholder="Email"
+              name="user_email"
+            />
+            <textarea
+              style={{
+                backgroundColor: darkMode && "#333",
+                color: darkMode && "white",
+              }}
+              type="text"
+              rows="5"
+              placeholder="Message"
+              name="message"
+            />
+            <button>Submit</button>
+            {done && "Thank you!"}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/data.js
@@ -0,0 +1,35 @@
+// import DarkPortfolio from "../src/img/DarkPortfolio.png"
+import LightPortfolio from "../src/img/LightPortfolio.png"
+
+export const products = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/10162332/pexels-photo-10162332.jpeg?cs=srgb&dl=pexels-maria-borisenko-10162332.jpg&fm=jpg",
+    link: "https://github.com/cesarmaaz",
+  },
+  {
+    id: 2,
+    img: {LightPortfolio},
+    link: "https://github.com/cesarmaaz",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "",
+  },
+];
