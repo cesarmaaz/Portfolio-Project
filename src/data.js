a558c0426d6a6e81537a9d493239e326895a6975
Cesar Maaz <cesarmaaz@gmail.com>
2022-02-10 20:56:58 +0200

    data

--- src/data.js
+++ src/data.js
@@ -1,6 +1,3 @@
-// import DarkPortfolio from "../src/img/DarkPortfolio.png"
-import LightPortfolio from "../src/img/LightPortfolio.png"
-
 export const products = [
   {
     id: 1,
@@ -9,27 +6,27 @@ export const products = [
   },
   {
     id: 2,
-    img: {LightPortfolio},
+    img: "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?cs=srgb&dl=pexels-designecologist-1779487.jpg&fm=jpg",
     link: "https://github.com/cesarmaaz",
   },
   {
     id: 3,
-    img: "",
-    link: "",
+    img: "https://images.pexels.com/photos/270360/pexels-photo-270360.jpeg?cs=srgb&dl=pexels-pixabay-270360.jpg&fm=jpg",
+    link: "https://github.com/cesarmaaz",
   },
   {
     id: 4,
-    img: "",
-    link: "",
+    img: "https://images.pexels.com/photos/1229861/pexels-photo-1229861.jpeg?cs=srgb&dl=pexels-hasan-albari-1229861.jpg&fm=jpg",
+    link: "https://github.com/cesarmaaz",
   },
   {
     id: 5,
-    img: "",
-    link: "",
+    img: "https://images.pexels.com/photos/997443/pexels-photo-997443.jpeg?cs=srgb&dl=pexels-tyler-lastovich-997443.jpg&fm=jpg",
+    link: "https://github.com/cesarmaaz",
   },
   {
     id: 6,
-    img: "",
-    link: "",
+    img: "https://images.pexels.com/photos/177598/pexels-photo-177598.jpeg?cs=srgb&dl=pexels-markus-spiske-177598.jpg&fm=jpg",
+    link: "https://github.com/cesarmaaz",
   },
 ];
